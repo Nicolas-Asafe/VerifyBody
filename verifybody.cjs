647c2bc9d13423f1ed06d8124546ae95214000b8
Nicolas-Asafe <nicolasasafe45@gmail.com>
2025-04-27 11:44:05 -0300

    Suporte para cjs e module

---
+++ verifybody.cjs
@@ -0,0 +1,41 @@
+// verifyBody.cjs (CommonJS)
+class Verify {
+  constructor(expectedBody = {}) {
+    this.expectedProperties = Object.keys(expectedBody);
+    this.expectedValues = expectedBody;
+  }
+
+  VerifyIfIsCorrect(body) {
+    const bodyProperties = Object.keys(body);
+
+    if (this.expectedProperties.length !== bodyProperties.length) {
+      return ['Credentials incorrect: different number of properties', false];
+    }
+
+    for (let prop of this.expectedProperties) {
+      if (!body.hasOwnProperty(prop)) {
+        return [`Property '${prop}' is missing`, false];
+      }
+
+      if (typeof body[prop] !== 'string') {
+        return [`Property '${prop}' must be a string`, false];
+      }
+    }
+
+    return ['Credentials are correct', true];
+  }
+
+  VerifyIfCredentialsExists(body) {
+    if (!body) {
+      return ['Credentials missing', false];
+    }
+    return ['Credentials found', true];
+  }
+
+  NewBody(body) {
+    this.expectedValues = body;
+    this.expectedProperties = Object.keys(body);
+  }
+}
+
+module.exports = Verify;
